73548826e5812afed6ba17a40aaa5b42c82a8814
divy451 <divymittal.123@gmail.com>
2025-06-13 15:04:22 +0530

    loader

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -18,7 +18,7 @@ const Index: React.FC = () => {
   const { data: categories, isLoading: categoriesLoading, error: categoriesError } = useCategories();
   const { data: categoryArticlesData, isLoading: categoryArticlesLoading, error: categoryArticlesError } = useAllCategoryArticles(categories || [], 5);
 
-  // Combine all loading states into one (still calculated for logging/debugging)
+  // Combine all loading states into one
   const isLoading = isLoadingBreaking || isLoadingFeatured || categoriesLoading || categoryArticlesLoading;
 
   useEffect(() => {
@@ -48,12 +48,11 @@ const Index: React.FC = () => {
 
   return (
     <MainLayout>
-      {/* Temporarily removed loader to test loading speed */}
-      {/* {isLoading ? (
+      {isLoading ? (
         <div className="fixed inset-0 flex justify-center items-center bg-white dark:bg-gray-800 z-50">
           <span className="loader"></span>
         </div>
-      ) : ( */}
+      ) : (
         <>
           {breakingError ? (
             <section className="mb-8 p-4 border border-red-200 rounded-lg bg-white">
@@ -131,7 +130,7 @@ const Index: React.FC = () => {
             </div>
           </div>
         </>
-      {/* )} */}
+      )}
     </MainLayout>
   );
 };
